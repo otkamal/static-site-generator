4658840fb8917f229a2b9cf44db13a06e61f1352
otkamal <okamal@duck.com>
2025-02-15 17:55:37 -0800

    added leafnode

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,7 +1,22 @@
 import htmlnode
+import warnings
 
 class LeafNode(htmlnode.HTMLNode):
 
-      def __init__(self, tag: str = None, value: str = None, props: dict = None):
-        if isinstance(tag, str) or tag is None:
-            self.tag = tag
+    def __init__(self, tag: str = None, value: str = None, props: dict = None):
+
+        if value is None:
+            raise TypeError("leafnode value must be set")
+
+        super().__init__(tag = tag, value = value, children = None, props = props)
+        
+    def to_html(self):
+
+        if self.value is None:
+            raise TypeError("leafnode value must be set")
+        
+        if self.tag is None:
+            return f"{self.value}"
+
+        return f"<{self.tag}>{self.value}</{self.tag}>" 
+        
